e71d12e1a94777119186a9ac7472949813f05455
mari <marianis22>
2020-01-02 16:47:58 -0600

    Primer comi

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { PortafolioComponent } from './pages/portafolio/portafolio.component';
+import { Expediente2Component } from './pages/expediente2/expediente2.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    PortafolioComponent,
+    Expediente2Component
   ],
   imports: [
     BrowserModule
